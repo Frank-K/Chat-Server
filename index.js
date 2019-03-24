353489241b0b689b03042324c253812b963c0322
Frank Karunaratna <frank.karunaratna@gmail.com>
2019-03-24 17:21:58 -0400

    add logging to the app

--- index.js
+++ index.js
@@ -3,35 +3,57 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.label({ label: '[my-label]' }),
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
 
 // Register public folder for js and css
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Send the homepage of the chat app
 app.get('/', (req, res) => {
+  logger.info(`visitor ${req.ip}`);
+
   res.sendFile(__dirname + '/home.html');
 });
 
 // Global users variable to keep track of current users
 global.users = {};
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
   
   // Notify all users of the new connection
-  socket.on('join', function(username) {
+  socket.on('join', (username) => {
+    logger.info(`connection ${socket.id} ${username}`);
+
     socket.broadcast.emit('server message', `${username} has joined the server.`);
     socket.emit('server message', `Welcome to the server ${username}!`);
     global.users[socket.id] = username;
   });
 
   // Notify all users of the disconnection
-  socket.on('disconnecting', function() {
+  socket.on('disconnecting', () => {
+    logger.info(`disconnection ${socket.id} ${global.users[socket.id]}`);
+
     socket.broadcast.emit('server message', `${global.users[socket.id]} has left the server.`);
     delete global.users[socket.id];
   });
 
   // Send the message to all users or send a message from the server to the user
-  socket.on('chat message', function(msg){
+  socket.on('chat message', (msg) => {
+    logger.info(`message ${global.users[socket.id]}: ${msg} `);
+
     if (msg.toString().length > 280) {
       socket.emit('server message', 'Sorry, your message was too long to send.');
     } else if (msg.toString() === '/u') {
@@ -41,7 +63,7 @@ io.on('connection', function(socket){
       socket.broadcast.emit('chat message', payload);
     }
   });
-  
+
 });
 
 http.listen(3000, () => {
