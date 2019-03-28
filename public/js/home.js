bbdda62b34b307086d7495555890da903dcdd620
Frank Karunaratna <frank.karunaratna@gmail.com>
2019-03-28 14:33:03 -0400

    change style for username popup

--- public/js/home.js
+++ public/js/home.js
@@ -8,10 +8,16 @@ $(function () {
 
   // Popup when user first visits the site
   Swal.fire({
-    title: 'Enter your name',
+    title: 'Enter your username',
     input: 'text',
+    customClass: 'shadow',
+    confirmButtonClass: 'shadow-sm',
     showCancelButton: false,
     allowOutsideClick: false,
+    confirmButtonText: 'start chatting',
+    confirmButtonColor: '#53ccfb',
+    backdrop: 'rgba(122, 199, 228, 0.50)',
+    background: '#f0f7ff',
     inputValidator: (value) => {
       if (!value) {
         return 'You need to choose a username!'
